e3dd032368d5ab2f9b6d1250ce72056f77b77999
AndB96 <andregit96@hotmail.com>
2024-12-17 10:42:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-console.log(db.pagination) // samtliga kategorier tillgänglig i db
+const toggleButton = document.getElementsByClassName("toggle-button")[0]; //Skapar variabeln toggleButton och hämtar första värdet i arrayen toggle-button
+const navbarLinks = document.getElementsByClassName("navbar-links")[0]; //Skapar variabeln navbarLinks och hämtar första värdet i arrayen navbar-links
+
+toggleButton.addEventListener("click", () => {
+  console.log("Toggle button clicked!");
+  navbarLinks.classList.toggle("active"); //Om så visas menyn
+  toggleButton.classList.toggle("active");
+});
+
+console.log(db.pagination); // samtliga kategorier tillgänglig i db
 
 //ex.
-console.log(db.bbqs) // lista med bbqs
+console.log(db.bbqs); // lista med bbqs
